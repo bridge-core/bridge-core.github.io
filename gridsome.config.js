0c90c3712582b991755fa370285f010dcaed045c
solvedDev <solvedDev@gmail.com>
2020-09-23 13:52:27 +0200

    Update gridsome.config.js

--- gridsome.config.js
+++ gridsome.config.js
@@ -97,12 +97,12 @@ module.exports = {
 			},
 		},
 
-		// {
-		// 	use: '@gridsome/plugin-google-analytics',
-		// 	options: {
-		// 		id: process.env.GA_ID ? process.env.GA_ID : 'XX-999999999-9',
-		// 	},
-		// },
+		{
+			use: '@gridsome/plugin-google-analytics',
+			options: {
+				id: process.env.GA_ID ? process.env.GA_ID : 'XX-999999999-9',
+			},
+		},
 
 		{
 			use: '@gridsome/plugin-sitemap',
