9513d62b5577f590673d98315a840f27a6b70c9a
Outer Cloud Studio <outercloudstudio@gmail.com>
2025-03-23 01:00:33 -0500

    Add redirect

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+window.location.replace('https://bridge-core.app/')
+
 // This is the main.js file. Import global CSS and scripts here.
 // The Client API can be used here. Learn more: gridsome.org/docs/client-api
 import DefaultLayout from '~/layouts/Default.vue'
